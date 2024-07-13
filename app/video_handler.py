5b61f790c7723cff11ff4e99e6ea742a1bca65fb
zafyyre <zaaf17@hotmail.com>
2024-07-13 15:42:50 -0700

    imported Request

--- app/video_handler.py
+++ app/video_handler.py
@@ -3,6 +3,7 @@ import io
 import json
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload
 
