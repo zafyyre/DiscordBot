fa6c693ec617557844ad923989cdcca405d42cf1
zafyyre <zaaf17@hotmail.com>
2024-07-13 09:33:06 -0700

    Modified main.py

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import discord
 from app.bot import run_bot
-
-# Create a Discord client instance
-intents = discord.Intents.default()
-intents.message_content = True
-client = discord.Client(intents=intents)
 
 if __name__ == "__main__":
     run_bot()
